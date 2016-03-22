434798cf8bf2cec4adda8e646646984b21b33e0a
Søren Bruus Frank <soerenbf@gmail.com>
2016-03-22 22:10:41 +0100

    reverted and added id count instead.

--- app/backend/dataModel.js
+++ app/backend/dataModel.js
@@ -10,6 +10,7 @@
     /* @ngInject */
     function DataModel() {
         var wines;
+        var idCount;
 
         this.getWines = getWines;
         this.getWineById = getWineById;
@@ -19,10 +20,11 @@
 
         function activate() {
             wines = [
-                {externalId: 144436, name: 'Bertani Valpolicella', year: 2011, varietal: 'Ripasso', rating: 3},
-                {externalId: 151951, name: 'Dom Perignon', year: 2003, varietal: 'Rosé', rating: 5},
-                {externalId: 131268, name: 'Cloudy Bay', year: 2012, varietal: 'Chardonnay', rating: 4}
+                {id: 1, externalId: 144436, name: 'Bertani Valpolicella', year: 2011, varietal: 'Ripasso', rating: 3},
+                {id: 2, externalId: 151951, name: 'Dom Perignon', year: 2003, varietal: 'Rosé', rating: 5},
+                {id: 3, externalId: 131268, name: 'Cloudy Bay', year: 2012, varietal: 'Chardonnay', rating: 4}
             ];
+            idCount = wines.length;
         }
 
         function getWines() {
@@ -30,7 +32,17 @@
         }
 
         function getWineById(id) {
-            return wines[id] || 'Could not find wine';
+            var wine;
+
+            for (var i = 0; i < wines.length; i++) {
+                wine = wines[i];
+
+                if (wine.id == id) {
+                    return wine;
+                }
+            }
+
+            return 'Could not find wine';
         }
 
         function addWine(wineObj) {
